7ea4599175d34844d2d4aacd17abdc91ecb80522
Federico Ojeda <ojedaf45@gmail.com>
2023-02-06 10:43:30 -0300

    Avances pagina web 1.0

--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def inicio(request):
     return render(request, 'AppCoder/inicio.html')
 
-# Create your views here.
-def curso(request):
-    curso = Curso(nombres ='Backend', camada='12345')
-    curso.save()
-    documentoDeTexto = f'Curso: {curso.nombres}, Camada: {curso.camada}'
-    
-    return HttpResponse(documentoDeTexto)
-    
+def cursos(request):
+    return render(request, 'AppCoder/cursos.html')
+
+def profesores(request):
+    return render(request, 'AppCoder/profesores.html')
+
+def estudiantes(request):
+    return render(request, 'AppCoder/estudiantes.html')
+
+def entregables(request):
+    return render(request, 'AppCoder/entregables.html')
